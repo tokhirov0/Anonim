28f3d34e4d621b9f9ecfc18995eed2a9c3cf58d6
tokhirov0 <tohirovoybek02@gmail.com>
2025-09-15 22:38:47 +0500

    config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.getenv("TOKEN", "8259267374:AAErvohz6-kXmwvlw6q7pdB6qk68JFMk_OY")
+OWNER = os.getenv("OWNER", "tokhirov_0")
+GROUP = os.getenv("GROUP", "anonimchat15")
+CHANNEL = os.getenv("CHANNEL", "anketaa_uz")
